ff71df51fb3baa71cb617eba73eb63033597b0c3
taica84 <taica_rules@yahoo.com>
2022-03-27 22:38:13 +0300

    testimonials section

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,55 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalClose = document.querySelectorAll('.services__modal');
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+};
+
+
+modalBtns.forEach((mb,i) => {
+    mb.addEventListener('click' , () => {
+        modal(i)
+    })
+});
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click' , () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
+        let mixerPortfolio = mixitup('.work__container', {
+            selectors: {
+                target: '.work__card'
+            },
+            animation: {
+                duration: 300
+            }
+        });
+
+
+
 
 /* Link active work */ 
+
+const linkWork = document.querySelectorAll('.work__item');
+
+function activeWork () {
+    linkWork.forEach(link => link.classList.remove('active__work'));
+    this.classList.add('active__work')
+};
+
+linkWork.forEach(link => link.addEventListener('click' , activeWork))
+
+
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
